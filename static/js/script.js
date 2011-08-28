06c4ad389ecf68b0d9f57bd02ecbec1d8ab96650
Carlos Rodriguez <carlos@s8f.org>
2011-08-27 20:41:21 -0700

    Broadcast sounds via click event.

--- static/js/script.js
+++ static/js/script.js
@@ -10,6 +10,8 @@
   
   var highlightSquares = app.highlightSquares = false;
 
+  var squares = app.squares = [];
+
   var initialize = app.initialize = function() {
     // Navigate according to the current hash.
     navigate();
@@ -50,18 +52,37 @@
     return _.template($('#template-' + name).text(), context || app);
   };
 
+  var playNote = app.playNote = function(note) {
+    var sound = $('#sound-' + note.sound).get(0);
+    if (!sound.paused) {
+      sound.pause();
+      sound.currentTime = 0.0;
+    }
+    sound.play();
+    var color = note.server ? '#300' : '#030';
+    squares[note.id].attr({fill: color}).animate({fill: '#000'}, 200, '<');
+  }
+
   var controller = app.controller = {
     home: function() {
-      $('#page').append(render('stage'));
+      $('#page').append(render('stage')).append(render('sounds'));
+      var socket = io.connect();
+      socket.on('note', function (note) {
+        playNote(note);
+      });
+
       var paper = Raphael('stage', 805, 505);
       for (var row = 0; row < 5; row++) {
         for (var col = 0; col < 8; col++) {
+          var note = {sound: 'clap'};
           var c = paper.rect(col * 100 + 5, row * 100 + 5, 100, 100)
             .attr({fill: '#000', stroke: '#0e0', 'stroke-dasharray': '. ', 'stroke-width': 1})
             .click(function() {
-            this.attr({fill: '#030'});
-            this.animate({fill: '#000'}, 200, '<');
-          });
+              note.id = this.id;
+              playNote(note);
+              socket.emit('note', note);
+            });
+          squares[c.id] = c;
         }
       }
     }
